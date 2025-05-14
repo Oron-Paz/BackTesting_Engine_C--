f513a4fd8e56a6fa1836be47d2001b6ec15fba2a
Oron Paz <oron.paz26@gmail.com>
2025-05-14 12:07:38 +0300

    what i said before

--- include/strategy_simulation.hpp
+++ include/strategy_simulation.hpp
@@ -2,18 +2,29 @@
 #include <vector>
 #include "csv_loader.hpp"
 
+namespace strategy{
+    enum class TradeAction { BUY, SELL, HOLD };
 
-struct TradeLog{
-    std::string date;
-    std::string action;
-    int shares;
-    double price;
-};
-
-double simulateRSIStrategy(const std::vector<csv_loader::PriceData>& data, 
-                           const std::vector<double>& rsi, 
-                           int rsiPeriod, 
-                           double startingCash, 
-                           double bottomThreshold,
-                           double topThreshold,
-                           std::vector<TradeLog>& logs);
+    struct TradeLog{
+        std::string date;
+        TradeAction action;
+        double current_cash;
+        double cash_after_action;
+        csv_loader::PriceData current_price_of_share;
+        int shares_owned_before_action;
+        int shares_bought;
+        int shares_sold;
+        int shares_owned_after_action;
+        double portfolio_value_after_action;
+    };
+    
+    double simulateRSIStrategy(const std::vector<csv_loader::PriceData>& data, 
+                               const std::vector<double>& rsi, 
+                               int rsiPeriod, 
+                               double startingCash, 
+                               double bottomThreshold,
+                               double topThreshold,
+                               std::vector<TradeLog>& logs,
+                               bool printLogs
+                            );
+}
